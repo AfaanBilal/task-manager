734ef59f570eda22bc60d1a294fa702a4696b195
Afaan Bilal <afaanbilal@gmail.com>
2023-06-27 22:07:26 +0530

    ✨ Implementing Sign Out

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/sign-up', [AuthController::class, 'signUp']);
 Route::post('/auth/sign-in', [AuthController::class, 'signIn']);
+Route::middleware('auth:sanctum')
+    ->post('/auth/sign-out', [AuthController::class, 'signOut']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
