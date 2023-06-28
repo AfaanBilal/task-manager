f60787972dae177895a72ba0f110baa63a56bcfc
Afaan Bilal <afaanbilal@gmail.com>
2023-06-28 13:15:27 +0530

    ✨ Implement Update Profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,6 +12,22 @@ class ProfileController extends Controller
     public function getProfile()
     {
         $user = Auth::user();
+
+        return $this->success([
+            'profile' => new UserResource($user),
+        ]);
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string']
+        ]);
+
+        /** @var User */
+        $user = Auth::user();
+        $user->name = $request->name;
+        $user->save();
 
         return $this->success([
             'profile' => new UserResource($user),
